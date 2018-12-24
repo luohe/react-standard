d27da4e89b86cfbb1e30f8ce86fd65c406078ff9
zhangzhuocheng <zhangzhuocheng@gagogroup.com>
2018-12-24 12:30:41 +0800

    docs(color-palette): colorPalette

--- src/color-palette.ts
+++ src/color-palette.ts
@@ -1,5 +1,7 @@
-
 import { globalColorPalette1 } from "@gago/frame/es/colors/default";
 import { GlobalColorPalette, generatorDerivativeColors } from "@gago/frame";
 
+/** 自定义颜色
+ * @author James Zhang
+ */
 export const colorPalette: GlobalColorPalette = { ...globalColorPalette1, mainColor: generatorDerivativeColors("#00a2b9") };
