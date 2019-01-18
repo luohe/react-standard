38ad33a428819e65483f5f5cdc1061319a75470e
张卓诚 <zhangzhuocheng@gagogroup.com>
2019-01-18 10:47:04 +0800

    feat(config-overrides.js): webpack-bundle-analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,13 @@
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer');
 
 module.exports = function override(config, env) {
   config = rewireReactHotLoader(config, env);
+  if (env === 'production') {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: 'static',
+      reportFilename: 'report.html'
+    })
+  }
   return config;
 }
