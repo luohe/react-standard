d8553a298d26f966b9539f36087437086d587aea
张卓诚 <zhangzhuocheng@gagogroup.com>
2019-03-06 16:51:57 +0800

    chore(package.json): update @gago/frame

--- src/store.ts
+++ src/store.ts
@@ -2,5 +2,5 @@ import { createStore, combineReducers, ReducersMapObject, Action } from "redux";
 import { rootReducer } from "./reducers";
 
 export const store = createStore(
-    combineReducers(rootReducer)
+    combineReducers(rootReducer),
 );
