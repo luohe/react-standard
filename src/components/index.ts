65d9d86718d5c320f8ccdd6535423901f4672a8f
张卓诚 <zhangzhuocheng@gagogroup.com>
2019-07-30 15:51:51 +0800

    chore(*): support export as lib

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { Loading } from "./loading";
+export { Todo } from "./todo";
+export { TodoList } from "./todo-list";
--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,5 +1,4 @@
 declare module "gago-react";
-declare module "tinycolor2";
 
 interface Window {
     /** extra config
